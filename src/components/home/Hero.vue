0bad4d84d54fd52781d8020ffb000ac45cd0a6e4
Danaila Catalin <hadesownage@gmail.com>
2020-12-12 22:56:17 +0200

    hero border bottom

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="bg-gray-50 border-t-2 border-gray-100">
+    <main class="bg-gray-50 border-t-2 border-b-2 border-gray-100">
         <div class="py-10 mx-auto max-w-7xl px-4 sm:py-8 sm:px-6 md:py-10 lg:py-12 lg:px-8 xl:py-16">
             <div class="sm:text-center">
                 <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl transition-all">
