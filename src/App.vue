2537eb57bdefa7cba775c24540de115cab1da77d
Danaila Catalin <hadesownage@gmail.com>
2020-12-12 22:10:01 +0200

    footer component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <Header />
   <router-view/>
+  <Footer />
 </template>
 
 <script>
 import Header from '@/components/header/Index.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'Home',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
