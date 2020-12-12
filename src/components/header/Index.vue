92647d6c2a4481c1b53f15eed07191ec1f7c8585
Danaila Catalin <hadesownage@gmail.com>
2020-12-12 21:33:45 +0200

    header component

---
+++ src/components/header/Index.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <Navbar />
+    <Hero />
+
+</template>
+
+<script>
+import Navbar from '@/components/header/Navbar.vue'
+import Hero from '@/components/header/Hero.vue'
+
+export default {
+    name: 'Header',
+    components: {
+        Navbar,
+        Hero
+    }
+}
+</script>
