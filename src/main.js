9b8c82e61be3ebb4ca243f5d32a9fbd89cd020a9
Danaila Catalin <hadesownage@gmail.com>
2020-12-13 16:55:28 +0200

    custom scrolbar

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/css/tailwind.css'
+import './assets/scss/custom.scss'
 import 'animate.css'
 
 createApp(App).use(router).mount('#app')
