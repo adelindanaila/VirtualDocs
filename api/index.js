6349a938bd58cb6ca272aa11c9a8721d9f7e025b
Danaila Catalin <hadesownage@gmail.com>
2020-12-13 21:59:42 +0200

    api setup

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const user = require('./routes/user')
+
+require('dotenv').config({ path: '../.env' })
+
+const app = express( )
+const port = process.env.PORT || 4000
+
+// middleware
+app.use(express.json( ))
+
+// database connection
+mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`, {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+
+}).then((result) => {
+
+    app.listen(port)
+    console.log(`connection established on :${port}`)
+
+}).catch((err) => console.log(err))
+
+// routes
+app.use(user)
