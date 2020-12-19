7bf37afb6cc352d254a7d9fd726e1960ad9980d1
Danaila Catalin <hadesownage@gmail.com>
2020-12-19 02:29:51 +0200

    add document api

--- api/controllers/document.js
+++ api/controllers/document.js
@@ -1,10 +1,10 @@
 const model = require('../models/document')
+const fs = require('fs')
 
 module.exports.documents = async ( request, response ) => {
 
     const { category, limit, page } = request.query
     
-
     try {
         
         const data = await model.documents( category, Number(limit), Number(page) )
@@ -22,7 +22,30 @@ module.exports.documents = async ( request, response ) => {
 
 module.exports.add = async ( request, response ) => {
 
-    console.log(request)
-    response.status(200).json({ data: 'test' })
+    const { file } = request
+    const { input0, input1, input2 } = request.body
+
+    try {
+
+        await fs.renameSync(file.path, `${file.path}.pdf`)
+        
+        const data = await model.create({
+
+            name: input0.trim( ),
+            description: input2.trim( ),
+            file: file.filename,
+            category: input1
+
+        })
+
+        response.status(200).json( data )
+
+    }
+
+    catch (error) {
+        
+        response.status(400).json( error )
+
+    }
 
 }
--- api/routes/document.js
+++ api/routes/document.js
@@ -1,10 +1,34 @@
 const { Router } = require('express')
+const multer = require('multer')
+
+require('dotenv').config({ path: '../../.env' })
+
+const upload = multer({
+    
+    dest: `../${process.env.UPLOAD_FOLDER}/`,
+    limits: {
+
+        fileSize: Number(process.env.UPLOAD_FILESIZE_LIMIT) * 1000000
+
+    },
+
+    fileFilter: function( req, file, cb ) {
+
+        if( file.mimetype !== 'application/pdf' )
+            return cb( new Error('Invalid file format') )
+
+        cb( null, true )
+
+    }
+
+})
+
 const controller = require('../controllers/document')
 const admin_authorization = require('../authorizations/admin')
 
 const router = Router( )
 
 router.get('/documents', controller.documents)
-router.post('/document/add', admin_authorization, controller.add)
+router.post('/document/add', [admin_authorization, upload.single('file')], controller.add)
 
 module.exports = router
