06a840b9253e9d2cabb0bf5377a279a11ce0f2d5
Danaila Catalin <hadesownage@gmail.com>
2020-12-20 01:15:41 +0200

    document acroforms

--- src/views/Document.vue
+++ src/views/Document.vue
@@ -1,30 +1,43 @@
 <template>
     <Hero />
-    <div class="max-w-7xl mx-auto py-8 bg-white">
-        <div class="flex items-center justify-between">
-			<h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-				<span class="block">You didn't find what you needed?</span>
-				<span class="block text-indigo-600">Contact us.</span>
-			</h2>
-			<div class="mt-8 lex lg:mt-0 lg:flex-shrink-0">
-				<div class="ml-3 inline-flex rounded-md shadow">
-					<a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">
-					Contact
-					</a>
-				</div>
-			</div>
-		</div>
-        <div class="flex items-center justify-center">
-            <canvas id="document" />
+    <div v-if="!loading" class="bg-white shadow-sm animate__animated animate__fadeIn animate__faster py-8">
+        <div class="max-w-7xl mx-auto flex items-center justify-between">
+            <h2 class="font-extrabold tracking-tight text-gray-900">
+                <span class="block text-3xl sm:text-4xl text-indigo-600">{{ document.name }}</span>
+                <span class="block text-xl">{{ document.description }}</span>
+            </h2>
+            <div class="mt-8 lex lg:mt-0 lg:flex-shrink-0">
+                <div class="ml-3 inline-flex rounded-md shadow">
+                    <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">
+                    Print
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
+    <div id="document_content" class="max-w-7xl mx-auto">
+        <div v-if="loading" id="loader_wrapper" class="w-full flex items-center justify-center animate__animated animate__fadeIn animate__faster">
+            <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+        </div>
+        <div v-show="!loading" class="animate__animated animate__fadeIn animate__faster py-16">
+            <div class="flex items-center justify-center">
+                <div id="document" />
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
 import Hero from '@/components/common/Hero'
 import axios from 'axios'
 
-const pdfjs = window.pdfjsLib
+import 'pdfjs-dist/web/pdf_viewer.css'
+import * as pdfjsLib from 'pdfjs-dist/build/pdf.js'
+import * as pdfjsViewer from 'pdfjs-dist/web/pdf_viewer.js'
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry.js'
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker
 
 export default {
     
@@ -37,33 +50,56 @@ export default {
 
     data: ( ) => ({
 
-        pdf: null,
-        page: 1,
-        page_rendering: false,
-        page_pending: null,
-        scale: 1.5,
-        canvas: null,
-        ctx: null
+        document: null,
+        loading: true
 
     }),
     
     async mounted( ) {
 
-        this.canvas = document.getElementById('document'),
-        this.ctx = this.canvas.getContext('2d')
+        const navbar = document.getElementById('navbar').offsetHeight
+		const hero = document.getElementById('hero').offsetHeight
+
+        document.getElementById('loader_wrapper').style.height = `calc(100vh - ${navbar + hero}px)`
+        document.getElementById('app').classList.add('bg-gray-200', 'bg-opacity-75')
 
         try {
 
             const params = new URLSearchParams({ document: this.$route.params.id }).toString( )
-            const document_data = await axios.get(`/document?${params}`)
+            const response = await axios.get(`/document?${params}`)
+            
+            this.document = response.data
 
-            const task = pdfjs.getDocument(`${this.api_url}/${this.upload_folder}/${document_data.data.file}.pdf`)
-            task.promise.then(async ( pdf ) => {
+            const task = pdfjsLib.getDocument(`${this.api_url}/${this.upload_folder}/${response.data.file}.pdf`)
+            task.promise.then(( pdf ) => {
+                
+                const _this = this
+                let promise = Promise.resolve( )
 
-                // console.log(pdf)
+                for( let i = 1; i <= pdf.numPages; i++ ) {
+                    promise = promise.then(
+                        function(pageNum) {
+                            return pdf.getPage(pageNum).then(function(pdfPage) {
 
-                this.pdf = pdf
-                await this.render_page( 1 )
+                                const pdfPageView = new pdfjsViewer.PDFPageView({
+                                    container: document.getElementById('document'),
+                                    id: pageNum,
+                                    scale: parseFloat(_this.document_viewport_scale),
+                                    defaultViewport: pdfPage.getViewport({
+                                        scale: parseFloat(_this.document_viewport_scale)
+                                    }),
+                                    eventBus: new pdfjsViewer.EventBus( ),
+                                    annotationLayerFactory: new pdfjsViewer.DefaultAnnotationLayerFactory( ),
+                                    renderInteractiveForms: true
+                                })
+
+                                pdfPageView.setPdfPage(pdfPage)
+                                _this.loading = false
+                                return pdfPageView.draw()
+                            })
+                        }.bind(null, i)
+                    );
+                }
 
             })
 
@@ -72,77 +108,15 @@ export default {
         catch( error ) {
 
             console.log( error )
+            this.$router.push('/404')
 
         }
 
     },
 
-    methods: {
+    unmounted( ) {
 
-        async render_page( page ) {
-
-            this.page_rendering = true   
-
-            const content = await this.pdf.getPage( page )
-            const viewport = content.getViewport( { scale: this.scale } )
-
-            this.canvas.height = viewport.height
-            this.canvas.width = viewport.width
-
-            // render pdf page into canvnas context
-            const render_context = {
-
-                canvasContext: this.ctx,
-                viewport
-
-            }
-
-            const render_task = content.render( render_context )
-
-            // wait for rendering to finish
-            render_task.promise.then(( ) => {
-
-                this.page_rendering = false
-
-                if( this.page_pending !== null ) {
-
-                    this.render_page( this.page_pending )
-                    this.page_pending = null
-
-                }
-
-            })
-
-            this.page = page
-
-        },
-
-        queue_render_page( page ) {
-
-            if( this.page_rendering )
-                this.page_pending = page
-            else
-                this.render_page( page )
-
-        },
-
-        previous_page( ) {
-
-            if( this.page <= 1 ) return
-
-            this.page--
-            this.queue_render_page( this.page )
-
-        },
-
-        next_page( ) {
-
-            if( this.page >= this.pdf.numPages ) return
-
-            this.page++
-            this.queue_render_page( this.page )
-
-        }
+        document.getElementById('app').classList.remove('bg-gray-200', 'bg-opacity-75')
 
     }
     
